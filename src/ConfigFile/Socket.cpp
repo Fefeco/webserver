090246ff876d4d6b516553a82b1aac464542cb01
Federico Carranza <fcarranz@cbr6s1.42barcelona.com>
2025-04-02 20:13:15 +0200

    Add constructor

--- src/ConfigFile/Socket.cpp
+++ src/ConfigFile/Socket.cpp
@@ -6,8 +6,39 @@
 /*   By: fcarranz <fcarranz@student.42barcelo>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 11:36:23 by fcarranz          #+#    #+#             */
-/*   Updated: 2025/04/01 11:36:24 by fcarranz         ###   ########.fr       */
+/*   Updated: 2025/04/02 20:10:54 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Socket.hpp"
+#include <exception>
+#include <sys/socket.h>
 
+Socket::Socket(void) {}
+
+Socket::Socket(int port) : _reuseOpt(1) {
+  _socketFd = socket(AF_INET, SOCK_STREAM, 0);
+
+  if (_socketFd < 0) {
+    exit(EXIT_FAILURE);
+  } // or throw exception
+
+  if (setsockopt(_socketFd, SOL_SOCKET, SO_REUSEADDR, &_reuseOpt, sizeof(int)) < 0) {
+    perror("setsockopt failed");
+    close(_socketFd);
+    throw new std::exception;
+  }
+
+  std::memset(_addressIn, 0, sizeof(_addressIn));
+  _addressIn.sin_family = AF_INET;
+  _addressIn.sin_port = htons(_port);
+  _addressIn.sin_addr.s_addr = INADDR_ANY;
+
+  if (bind(_socketFd, (struct sockaddr *)&_addressIn, sizeof(_addressIn)) < 0) {
+    perror("Socket bind failed");
+    close(_socketFd);
+    throw new std::exception;
+  }
+}
+
+~Socket(void) {}
--- src/ConfigFile/Socket.hpp
+++ src/ConfigFile/Socket.hpp
@@ -6,68 +6,21 @@
 /*   By: fcarranz <fcarranz@student.42barcelo>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 11:36:30 by fcarranz          #+#    #+#             */
-/*   Updated: 2025/04/01 11:45:07 by fcarranz         ###   ########.fr       */
+/*   Updated: 2025/04/02 20:12:09 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include <sys/socket.h>
-/*
 
-void						Server::socketUp()
-{
-	_serverFd = socket(AF_INET, SOCK_STREAM, 0);
-	_reuseOption = 1;
-// Poder reutilizar el mismo puerto sin tener que esperar en caso de fallo del programa
-	if (setsockopt(_serverFd, SOL_SOCKET, SO_REUSEADDR, &_reuseOption, sizeof(int)) == -1) {
-		perror("setsockopt failed");
-		close(_serverFd);
-		throw new std::exception;
-	}
-	if (fcntl(_serverFd, F_SETFL, O_NONBLOCK) == -1) {
-        perror("fcntl failed");
-		close(_serverFd);
-		throw new std::exception;
-	}
-// carga el host name y port en el addrinfo
-// hints -> asignamos opciones de config
-	memset(&_hints, 0, sizeof(_hints));
-	_hints.ai_family = AF_INET;			// IPv4
-	_hints.ai_socktype = SOCK_STREAM;	// TCP
-	_hints.ai_flags = AI_PASSIVE;		// Para bind() (escuchar)
-// con las opciones dadas en hints configura res
-	int err = getaddrinfo(getHostName().c_str(), getPort().c_str(), &_hints, &_response);
-	if (err) {
-		std::cout << "error: getaddrinfo" << std::endl << gai_strerror(err) << std::endl;
-		return ;
-	}
-// vincula fd del servidor al host name y port
-	else if (bind(_serverFd, _response->ai_addr, _response->ai_addrlen) == -1) {
-		std::cout << "error: bind" << std::endl;
-		return ;
-	}
-// habilitamos el fd para que se quede escuchando
-	if (listen(_serverFd, SOMAXCONN) == -1) {
-		perror("listen failed");
-		close(_serverFd);
-		throw new std::exception;
-	}
-}
-*/
 class Socket {
 public:
-  Socket(void);
   Socket(int port);
   ~Socket(void);
 
 private:
+  Socket(void);
 	int _socketFd;
+	int _reuseOpt;
+  struct sockaddr_in _addressIn;
 };
-
-/*
-		//SocketUp
-		int					_serverFd;
-		int					_reuseOption;
-		struct addrinfo*	_response;
-		struct addrinfo		_hints;
-*/
